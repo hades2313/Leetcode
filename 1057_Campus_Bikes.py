4c76ee7aa1a3b42390868e48951c9aa368726e0b
hades2313 <wythhh@gmail.com>
2019-11-03 21:28:20 -0500

    update 1057

--- 1057_Campus_Bikes.py
+++ 1057_Campus_Bikes.py
@@ -25,7 +25,7 @@ class Solution(object):
                 break
         return result
 
-# time: O(mn + nlogn + mn)
+# time: O(mn + mnlogn + mnlogn)
 import heapq
 class Solution(object):
     def assignBikes(self, workers, bikes):
@@ -34,25 +34,24 @@ class Solution(object):
         :type bikes: List[List[int]]
         :rtype: List[int]
         """
-        m, n = len(workers), len(bikes)
-        distance_list = [[] for _ in range(m)]
-        used_bikes = set()
-        result = [-1] * len(workers)
-        for i, worker in enumerate(workers):
-            for j, bike in enumerate(bikes):
-                distance = abs(worker[0] - bike[0]) + abs(worker[1] - bike[1])
-                distance_list[i].append((distance, i, j))
-            distance_list[i].sort()
+        m, n = len(bikes), len(workers)
+        result = [-1] * n
+        distances = [[] for _ in range(n)]
+        candidates = []
+        for i in range(n):
+            for j in range(m):
+                distance = abs(workers[i][0] - bikes[j][0]) + abs(workers[i][1] - bikes[j][1])
+                distances[i].append((distance, j))
+            distances[i].sort()
+            candidates.append((distances[i][0][0], i, 0))
+        heapq.heapify(candidates)
 
-        heap = [(distance_list[i][0][0], distance_list[i][0][1], distance_list[i][0][2], 0) for i in range(m)]
-        heapq.heapify(heap)
-        used_workers_count = 0
-        while used_workers_count < m:
-            _, worker_idx, bike_idx, pos = heapq.heappop(heap)
-            if bike_idx in used_bikes:
-                heapq.heappush(heap, (distance_list[worker_idx][pos + 1][0], distance_list[worker_idx][pos + 1][1], distance_list[worker_idx][pos + 1][2], pos + 1))
+        paired_bikes = set()
+        while len(paired_bikes) < n:
+            distance, worker_idx, bike_pos = heapq.heappop(candidates)
+            if distances[worker_idx][bike_pos][1] in paired_bikes:
+                heapq.heappush(candidates, (distances[worker_idx][bike_pos + 1][0], worker_idx, bike_pos + 1))
                 continue
-            result[worker_idx] = bike_idx
-            used_bikes.add(bike_idx)
-            used_workers_count += 1
+            paired_bikes.add(distances[worker_idx][bike_pos][1])
+            result[worker_idx] = distances[worker_idx][bike_pos][1]
         return result
